9faf02dd34032a59953924a030050467758fa140
byankatm <byankatomaz@gmail.com>
2023-09-06 11:29:22 -0300

    update

--- Projeto_SAC/SAC/App_SAC/urls.py
+++ Projeto_SAC/SAC/App_SAC/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('cons_cliente/', views.cons_cliente, name='cons_cliente'),
     path('edit_cliente/<int:id>', views.edit_cliente, name='edit_cliente'),
     path('salvar_cliente_editado', views.salvar_cliente_editado, name='salvar_cliente_editado'),
-    path('delete_cliente/<int:id>', views.delete_cliente, name='delete_cliente')
+    path('delete_cliente/<int:id>', views.delete_cliente, name='delete_cliente'),
+    path('cad_atend', views.cad_atend, name='cad_atend'),
+    path('salvar_atend_novo', views.salvar_atend_novo, name='salvar_atend_novo'),
+    path('cons_atend', views.cons_atend, name='cons_atend')
 
 ]
--- Projeto_SAC/SAC/App_SAC/views.py
+++ Projeto_SAC/SAC/App_SAC/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from .models import Cliente
+from .models import Cliente, Atendente
 from django.contrib import messages
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 @login_required
 def abrir_index(request):
@@ -120,3 +121,82 @@ def delete_cliente(request, id):
     
     messages.info(request, 'Cliente ' + nome + ' excluido com sucesso')
     return redirect('cons_cliente', {'usuario_logado': usuario_logado})
+
+
+@login_required
+def salvar_atend_novo(request):
+    usuario_logado = request.user.username
+    
+    if (request.method == 'POST'):
+        nome_atend = request.POST.get('nome_atend')
+        telefone_atend = request.POST.get('telefone_atend')
+        user_atend = request.POST.get('user_atend')
+        observacao_atend = request.POST.get('observacao_atend')
+        
+        if user_atend:
+            user_atend = User.objects.get(username=user_atend)
+        else:
+            user_atend = None
+        
+        grava_atend = Atendente(
+            nome_atend=nome_atend,
+            telefone_atend=telefone_atend,
+            observacao_atend=observacao_atend,
+            ativo_atend=1,
+            user_atend=user_atend
+        )
+        
+        grava_atend.save()
+        messages.info(request, 'Atendente ' + nome_atend + ' cadastrado com sucesso!', 'cad_atend')
+        cons_users = User.objects.all()
+        return render(request, 'Cad_Atendente.html', {'usuario_logado': usuario_logado, 'cons_users': cons_users})
+    
+    
+@login_required
+def cad_atend(request):
+    cons_users = User.objects.all()
+    usuario_logado = request.user.username
+    
+    return render(request, 'Cad_Atendente.html', {'usuario_logado': usuario_logado, 'cons_users': cons_users})
+
+@login_required
+def cons_atend(request):
+    dado_pesquisa_atendente = request.POST.get('atendente')
+    dado_pesquisa_todos = request.POST.get('seleciona_todos')
+    
+    usuario_logado = request.user.username
+    
+    if dado_pesquisa_todos == 'N' and dado_pesquisa_atendente != None:
+        todos_atendentes = Atendente.objects.filter(nome_atend__icontains=dado_pesquisa_atendente)
+    
+    elif dado_pesquisa_todos == 'S' and dado_pesquisa_atendente != None:
+        todos_atendentes = Atendente.objects.filter(nome_atend__icontains=dado_pesquisa_atendente, ativo_atend=1)
+    
+    elif dado_pesquisa_todos == 'N' and dado_pesquisa_atendente == None:
+        todos_atendentes = Atendente.objects.all()
+        
+    else:
+        todos_atendentes = Atendente.objects.filter(ativo_atend=1)
+    
+    page = request.GET.get('page')
+    
+    if page:
+        dado_pesquisa = request.GET.get('dado_pesquisa')
+        atendentes_lista = Atendente.objects.filter(nome_atend__icontains=dado_pesquisa)
+        paginas = Paginator(atendentes_lista, 3)
+        page = request.GET.get(page)
+        atendentes = paginas.get_page(page)
+        
+        return render(request, 'Cons_Atendente.html', {'todos_atendentes': atendentes, 'dado_pesquisa': dado_pesquisa, 'usuario_logado': usuario_logado})
+    
+    if dado_pesquisa_atendente != None and dado_pesquisa_atendente != '':
+        atendentes_lista = Atendente.objects.filter(nome_atend__icontains=dado_pesquisa_atendente)
+        paginas = Paginator(atendentes_lista, 3)
+        page = request.GET.get(page)
+        atendentes = paginas.get_page(page)
+        
+        return render(request, 'Cons_Atendente.html', {'todos_atendentes': atendentes, 'dado_pesquisa': dado_pesquisa_atendente, 'usuario_logado': usuario_logado})
+    else:
+        return render(request, 'Cons_Atendente.html', {'todos_atendentes': todos_atendentes, 'usuario_logado': usuario_logado})
+    
+        
